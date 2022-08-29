fb71af8d411eb8d46bd5de308ccc2017da7811b3
Nalinda Wickramarathna <mastersilicon@gmail.com>
2022-08-29 16:35:28 +0530

    News list basic view controller setup

---
+++ GoodNews/Controllers/NewsListViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  GoodNews
+//
+//  Created by Nalinda Wickramarathna on 8/29/22.
+//
+
+import UIKit
+
+class NewsListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+    }
+    
+    private func setup() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
+
+extension NewsListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
+
